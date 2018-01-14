2af1fa813f1c26515b31d9f0693bd984afacbbe1
RIM <navyfox8@gmail.com>
2018-01-15 03:22:17 +0600

    Add text area

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
 import './List.css';
-import TaskList from '../TaskList/TaskList';
 
 class List extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            addCard: false,
+            obj: {
+                item1: "Lorem ipsum dolor sit amet",
+                item2: "Lorem ipsum dolor sit amet",
+                item3: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet"
+            }
+        };
+        // var obj = {
+        //     item1: "Lorem ipsum dolor sit amet",
+        //     item2: "Lorem ipsum dolor sit amet",
+        //     item3: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet"
+        // };
+        // var serialObj = JSON.stringify(obj);
+        // localStorage.setItem("myKey", serialObj);
+        // var returnObj = JSON.parse(localStorage.getItem("myKey"));
+    }
+    rendDefault() {
         return (
             <div className="list">
                 <header>{this.props.title}</header>
                 <ul>
-                    <TaskList>Lorem ipsum dolor sit amet</TaskList>
-                    <TaskList>Lorem ipsum dolor sit amet</TaskList>
-                    <TaskList>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</TaskList>
-                    <TaskList>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</TaskList>
+                    <li>{this.state.obj.item1}</li>
+                    <li>{this.state.obj.item2}</li>
+                    <li>{this.state.obj.item3}</li>
                 </ul>
-                <footer>Add a card...</footer>
+                <footer><a onClick={() => this.setState({addCard: true})} className="add-card">Add a card...</a></footer>
             </div>
         )
+    }
+    rendAdd() {
+        return (
+            <div className="list">
+                <header>{this.props.title}</header>
+                <ul>
+                    <li>{this.state.obj.item1}</li>
+                    <li>{this.state.obj.item2}</li>
+                    <li>{this.state.obj.item3}</li>
+                    <li><textarea defaultValue="My Card"></textarea></li>
+                </ul>
+                <footer>
+                    <input value="Add" type="submit"/>
+                    <input value="Delete" type="submit" onClick={() => this.setState({addCard: false})}/>
+                </footer>
+            </div>
+        )
+    }
+    render() {
+        if (this.state.addCard) {
+            return (this.rendAdd())
+        } else {
+            return (this.rendDefault())
+        }
     }
 }
 
