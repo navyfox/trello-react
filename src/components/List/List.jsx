2bc22bb8697aad35845bad9a15b7139dd4a05583
RIM <navyfox8@gmail.com>
2018-01-16 16:33:03 +0600

    Add task state

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -6,12 +6,17 @@ class List extends Component {
         super(props);
         this.state = {
             addCard: false,
+            value: '',
             obj: {
                 item1: "Lorem ipsum dolor sit amet",
                 item2: "Lorem ipsum dolor sit amet",
                 item3: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet"
-            }
+            },
+            loc: ["Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet"],
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         // var obj = {
         //     item1: "Lorem ipsum dolor sit amet",
         //     item2: "Lorem ipsum dolor sit amet",
@@ -21,14 +26,20 @@ class List extends Component {
         // localStorage.setItem("myKey", serialObj);
         // var returnObj = JSON.parse(localStorage.getItem("myKey"));
     }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    handleSubmit(event) {
+        this.state.loc.push(this.state.value);
+        this.setState({addCard: false});
+        // alert('Textarea value is: ' + this.state.value);
+    }
     rendDefault() {
         return (
             <div className="list">
                 <header>{this.props.title}</header>
                 <ul>
-                    <li>{this.state.obj.item1}</li>
-                    <li>{this.state.obj.item2}</li>
-                    <li>{this.state.obj.item3}</li>
+                    {this.state.loc.map((item, index) => <li key={index}>{item}</li>)}
                 </ul>
                 <footer><a onClick={() => this.setState({addCard: true})} className="add-card">Add a card...</a></footer>
             </div>
@@ -39,13 +50,16 @@ class List extends Component {
             <div className="list">
                 <header>{this.props.title}</header>
                 <ul>
-                    <li>{this.state.obj.item1}</li>
-                    <li>{this.state.obj.item2}</li>
-                    <li>{this.state.obj.item3}</li>
-                    <li><textarea defaultValue="My Card"></textarea></li>
+                    {this.state.loc.map((item, index) => <li key={index}>{item}</li>)}
+                    <li><textarea
+                        name="description"
+                        // value={this.state.value}
+                        onChange={this.handleChange}
+                    />
+                    </li>
                 </ul>
                 <footer>
-                    <input value="Add" type="submit"/>
+                    <input value="Add" type="submit" onClick={this.handleSubmit}/>
                     <input value="Delete" type="submit" onClick={() => this.setState({addCard: false})}/>
                 </footer>
             </div>
